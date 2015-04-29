714b59d801e6045a2afa78cda1558070857492a8
Lucas <lucas.girardin@outlook.com>
2015-04-29 20:06:59 +0200

    sans gitugnore

---
+++ WEB-INF/src/render/RendererJSON.java
@@ -0,0 +1,124 @@
+package render;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import org.esgi.web.framework.context.interfaces.IContext;
+import org.esgi.web.framework.renderer.interfaces.IRenderer;
+
+/**
+ * JSON renderer. The rendered object is taken in the <b>model</b> field in the context
+ * attributes. The object is displayed by its public fields using reflexivity
+ * 
+ * @author Vuzi
+ *
+ */
+public class RendererJSON implements IRenderer {
+
+	@Override
+	public String render(IContext context) {
+		try {
+			Object model = context.getAttribute("model");
+
+			if (model != null)
+				return renderObject(model);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return "";
+	}
+
+	/**
+	 * Render array using reflexivity
+	 * @param model The object to display
+	 * @return The rendered object
+	 * @throws Exception
+	 */
+	public String renderArray(Object model) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		boolean first = true;
+		
+		if (model != null) {
+			sb.append(" [ ");
+			
+			int length = Array.getLength(model);
+		    for (int i = 0; i < length; i ++) {
+		    	
+				if(first)
+					first = false;
+				else
+					sb.append(", ");
+				
+		        sb.append(renderObject(Array.get(model, i)));
+		    }
+			
+			
+			sb.append(" ] ");
+		}
+		
+		return sb.toString();
+	}
+	
+	/**
+	 * Render object using reflexivity
+	 * @param model The object to display
+	 * @return The rendered object
+	 * @throws Exception
+	 */
+	public String renderObject(Object model) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		boolean first = true;
+		
+		if (model != null) {
+			if(model.getClass().isPrimitive()) {
+				// Primitive
+				sb.append(" " + model + " ");
+			} else if (model.getClass().equals(String.class)) {
+				// String
+				sb.append("\"" + model + "\"");
+			} else if (model.getClass().equals(Integer.class) || model.getClass().equals(Long.class) || model.getClass().equals(Double.class) || model.getClass().equals(Boolean.class)) {
+				// Numerical value
+				sb.append(" " + model + " ");
+			} else if(model.getClass().isArray()) {
+				return renderArray(model);
+			} else if(model.getClass().equals(Date.class)) {
+				// Java date
+				Date d = (Date) model;
+				DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+				sb.append("\"" + df.format(d) + "\"");
+			} else {
+				// Other object
+				sb.append("{ ");
+	
+				for (Field field : model.getClass().getFields()) {
+					String key = "\"" + field.getName() + "\"";
+					String value = renderObject(field.get(model));
+	
+					// Display field values
+					if(first)
+						first = false;
+					else
+						sb.append(",");
+	
+					sb.append(" " + key + " : " + value);
+				}
+	
+				sb.append(" }");
+			}
+		} else {
+			return "null";
+		}
+
+		return sb.toString();
+	}
+
+	public String getHttpType() {
+		return "application/json; charset=UTF-8";
+	}
+	
+}
