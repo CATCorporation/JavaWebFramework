3ebba51871d471a45b49524c7a52dae03f744459
Emerich IMBART <la_muerte_97216@hotmail.com>
2015-06-05 14:52:05 +0200

    commit forcé

--- insert.sql
+++ insert.sql
@@ -1,3 +1,10 @@
+
+--insert into role(name_role) values ("admin");
+--insert into role(name_role) values ("user");
+
+insert into user(login, password) values("admin", "admin",1);
+insert into user(login, password) values("user", "user",2); 
+
 insert into news(title_news, text_news, id_user) values ("Derni&egrave;re actualit&eacute;", "Et Epigonus quidem amictu tenus philosophus, ut apparuit, prece frustra temptata, sulcatis lateribus mortisque metu admoto turpi confessione cogitatorum socium, quae nulla erant, fuisse firmavit cum nec vidisset quicquam nec audisset penitus expers forensium rerum; Eusebius vero obiecta fidentius negans, suspensus in eodem gradu constantiae stetit latrocinium illud esse, non iudicium clamans.
 
 Intellectum est enim mihi quidem in multis, et maxime in me ipso, sed paulo ante in omnibus, cum M. Marcellum senatui reique publicae concessisti, commemoratis praesertim offensionibus, te auctoritatem huius ordinis dignitatemque rei publicae tuis vel doloribus vel suspicionibus anteferre. Ille quidem fructum omnis ante actae vitae hodierno die maximum cepit, cum summo consensu senatus, tum iudicio tuo gravissimo et maximo. Ex quo profecto intellegis quanta in dato beneficio sit laus, cum in accepto sit tanta gloria.
@@ -15,9 +22,3 @@ insert into news(title_news, text_news, id_user) values ("Actualit&eacute; avant
 Intellectum est enim mihi quidem in multis, et maxime in me ipso, sed paulo ante in omnibus, cum M. Marcellum senatui reique publicae concessisti, commemoratis praesertim offensionibus, te auctoritatem huius ordinis dignitatemque rei publicae tuis vel doloribus vel suspicionibus anteferre. Ille quidem fructum omnis ante actae vitae hodierno die maximum cepit, cum summo consensu senatus, tum iudicio tuo gravissimo et maximo. Ex quo profecto intellegis quanta in dato beneficio sit laus, cum in accepto sit tanta gloria.
 
 Horum adventum praedocti speculationibus fidis rectores militum tessera data sollemni armatos omnes celeri eduxere procursu et agiliter praeterito Calycadni fluminis ponte, cuius undarum magnitudo murorum adluit turres, in speciem locavere pugnandi. neque tamen exiluit quisquam nec permissus est congredi. formidabatur enim flagrans vesania manus et superior numero et ruitura sine respectu salutis in ferrum.", 1);
-
--- insert into role(name_role) values ("admin");
--- insert into role(name_role) values ("user");
-
--- insert into user(login, password) values("admin", "admin");
--- insert into user(login, password) values("user", "user");
